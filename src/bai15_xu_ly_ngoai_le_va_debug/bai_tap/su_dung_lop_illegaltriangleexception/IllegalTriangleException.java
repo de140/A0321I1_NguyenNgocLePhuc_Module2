14bc2bb5dbe38561354d641860d7a293c5ada132
A0321I1-NguyenNgocLePhuc <phucnnlde140045@fpt.edu.vn>
2021-08-15 16:37:54 +0700

    nop bai

---
+++ src/bai15_xu_ly_ngoai_le_va_debug/bai_tap/su_dung_lop_illegaltriangleexception/IllegalTriangleException.java
@@ -0,0 +1,14 @@
+package bai15_xu_ly_ngoai_le_va_debug.bai_tap.su_dung_lop_illegaltriangleexception;
+
+public class IllegalTriangleException extends Exception {
+    private String msg;
+
+    public IllegalTriangleException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public IllegalTriangleException() {
+        super("Invalid!");
+    }
+}
