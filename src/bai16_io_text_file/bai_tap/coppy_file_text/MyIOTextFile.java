fda615bafde645d63e84524e3592bb25c7b7cfa9
A0321I1-NguyenNgocLePhuc <phucnnlde140045@fpt.edu.vn>
2021-08-22 15:42:41 +0700

    nop bai

---
+++ src/bai16_io_text_file/bai_tap/coppy_file_text/MyIOTextFile.java
@@ -0,0 +1,4 @@
+package bai16_io_text_file.bai_tap.coppy_file_text;
+
+public class MyIOTextFile {
+}
---
+++ src/bai17_binary_file_serrialization/bai_tap/quan_ly_san_pham_luu_ra_fule_nhi_phan/Main.java
@@ -0,0 +1,7 @@
+package bai17_binary_file_serrialization.bai_tap.quan_ly_san_pham_luu_ra_fule_nhi_phan;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductManager.mainMenu();
+    }
+}
